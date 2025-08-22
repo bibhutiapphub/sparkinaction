9930f8f5d1b01101fbd6b3e7366d151b4d080e69
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-08-22 11:18:40 +0530

    SparkSession intro and Datframe creation

---
+++ sparkintro/dataframe_intro.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("DataFrame Intro") \
+    .getOrCreate()
+
+my_df = spark.read \
+    .format("csv") \
+    .option("header","true") \
+    .option("inferSchema","true") \
+    .load("../input_data/fire_dept_calls.csv")
+
+my_df.createGlobalTempView("fire_service_data")
+
+from_view_df = spark.sql("select * from global_temp.fire_service_data")
+from_view_df.show()

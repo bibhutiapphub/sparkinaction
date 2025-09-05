8db06e8c43b3ecd35709b2b7f78c55dbe502f5e9
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-09-05 18:11:10 +0530

    Spark DataFrame Writer

--- data_sources/dataframe_write_layout.py
+++ data_sources/dataframe_write_layout.py
@@ -5,12 +5,14 @@ if __name__ == "__main__":
              .builder
              .master("local[2]")
              .appName("SparkWriterDemo")
+             .enableHiveSupport()
              .getOrCreate())
 
     flight_data_df = (spark.read
                       .format("parquet")
                       .load("../input_data/flight-time.parquet"))
 
+    # Write it as files
     (flight_data_df.write
      .format("parquet")
      .mode("overwrite")
@@ -18,3 +20,16 @@ if __name__ == "__main__":
      .option("maxRecordsPerFile","10000")
      .option("path", "../data_sink/parquet/")
      .save())
+
+    # Write it as table
+    spark.sql("CREATE DATABASE IF NOT EXISTS airline_db")
+
+    (flight_data_df.write
+     .mode("overwrite")
+     .saveAsTable("airline_db.flight_times"))
+
+    is_table_exists = spark.catalog.tableExists("airline_db.flight_times")
+    print(is_table_exists)
+    print(spark.catalog.listTables("airline_db"))
+    flight_time_df = spark.sql("SELECT * FROM airline_db.flight_times")
+    flight_time_df.show()

4fca19e39bd8e22d67734c4c6b1f2fda50af6373
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-09-06 12:11:08 +0530

    Bucketed By demo

--- data_sources/dataframe_write_layout.py
+++ data_sources/dataframe_write_layout.py
@@ -13,19 +13,24 @@ if __name__ == "__main__":
                       .load("../input_data/flight-time.parquet"))
 
     # Write it as files
-    (flight_data_df.write
-     .format("parquet")
-     .mode("overwrite")
-     .partitionBy("OP_CARRIER", "ORIGIN") # This benefits in partition elimination.
-     .option("maxRecordsPerFile","10000")
-     .option("path", "../data_sink/parquet/")
-     .save())
+    # (flight_data_df.write
+    #  .format("parquet")
+    #  .mode("overwrite")
+    #  .partitionBy("OP_CARRIER", "ORIGIN") # This benefits in partition elimination.
+    #  .option("maxRecordsPerFile","10000")
+    #  .option("path", "../data_sink/parquet/")
+    #  .save())
 
     # Write it as table
     spark.sql("CREATE DATABASE IF NOT EXISTS airline_db")
 
     (flight_data_df.write
      .mode("overwrite")
+     # .partitionBy("OP_CARRIER", "ORIGIN")  # This benefits in partition elimination. But results in large number of partitions
+     # Especially with columns with unique values results in large number of partitions
+     # Same can be achieved by bucketBy function.
+     .bucketBy(5, "OP_CARRIER", "ORIGIN")
+     .sortBy("OP_CARRIER", "ORIGIN")
      .saveAsTable("airline_db.flight_times"))
 
     is_table_exists = spark.catalog.tableExists("airline_db.flight_times")
